a55bd2fa51ff8b0b9cffc23a82b1eb0e5b037ab8
hodaya55 <37270283+hodaya55@users.noreply.github.com>
2018-06-20 14:16:39 +0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -10,29 +10,13 @@ let eventsHandler = new EventsHandler(postsRenderer, userRepository);
 
 console.log('im in main');
 
-// get all the users
-<<<<<<< HEAD
-// in order to check if user already exist in db
-userRepository.getUsers();
-
-console.log('users array');
-console.log( userRepository.users);
-
-
-
-
-eventsHandler.registerAddUser();
-=======
 // in order to check if user exist or not in db
 userRepository.getUsers();
 
 eventsHandler.registerAddUser();
 eventsHandler.registerLogIn();
 
-// let userObj=JSON.parse(localStorage.getItem('user'));
-// $('.helloUser').text(`Hello ${userObj.userName}`);
-
 eventsHandler.registerLoggedOut();
->>>>>>> 1523a64ef72d6653bd7bbb9d8a59d3e2728e560c
+
 eventsHandler.registerAddPost();
 
